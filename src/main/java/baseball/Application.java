7be54537da15db5874c724dc942fe40a2b4a02d2
apio1130 <apio1130@naver.com>
2022-04-17 22:46:15 +0900

    feat(game): 숫자 야구 게임 프로그램 구현
    - 어플리케이션에 숫자 야구 게임 프로그램 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import baseball.domain.Computer;
+import baseball.domain.Player;
+import baseball.domain.Referee;
+import baseball.game.NumbersBaseballGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 프로그램 구현
+        Computer computer = new Computer(); // 컴퓨터
+        Referee referee = new Referee();    // 삼핀
+        Player player = new Player();       // 사용자
+        // 숫자 야구 게임 시작
+        NumbersBaseballGame game = new NumbersBaseballGame(computer, referee, player);
+        game.play();    // 게임 시작
+        while (game.isContinue()) { // 재시작 여부 체크
+            game.play();
+        }
     }
 }
